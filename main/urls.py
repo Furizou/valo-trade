783d2071a17e8db427f7d4e60b017dcbaa99ffb1
Kukuh Cikal <71339508+Furizou@users.noreply.github.com>
2024-11-19 17:32:54 +0700

    feat: flutter auth & integration with django

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, add_product_ajax 
+from main.views import create_product_flutter, show_main, create_product, add_product_ajax 
 from main.views import show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import edit_product, delete_product
 from main.views import register, login_user, logout_user
@@ -19,4 +19,5 @@ urlpatterns = [
     path('edit-product/<uuid:id>', edit_product, name='edit_product'),
     path('delete/<uuid:id>', delete_product, name='delete_product'),
     path('create-product-ajax', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- valo_trade/settings.py
+++ valo_trade/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1$^@i&qz5u@vq6smjx2^7c-6st_u6k1duz*5t%@9ya27gm2uo-
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kukuh-cikal-valotrade.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kukuh-cikal-valotrade.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'valo_trade.urls'
@@ -134,3 +137,11 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://kukuh-cikal-valotrade.pbp.cs.ui.ac.id/", "https://kukuh-cikal-valotrade.pbp.cs.ui.ac.id/"]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
